ab8ff15aa00f9e0d5997be7f0dadf268c97a1faa
AhmerRaza12 <ahmer183@gamerz@gmail.com>
2024-05-03 02:07:43 +0500

    changes

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -194,14 +194,14 @@ const Footer = () => {
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <div className="sm:flex sm:justify-evenly justify-evenly ">
-          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400 sm:text-xs">
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400 ">
             © 2022{" "}
             <a href="/" className="hover:underline">
               DSL Courier™
             </a>
             . All Rights Reserved.
           </span>
-          <div className="flex mt-4 space-x-6 sm:justify-evenly sm:mt-0">
+          <div className="flex  space-x-6 sm:justify-evenly sm:mt-0">
             <a
               href="https://www.facebook.com/dslcourier"
               className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
