2abb17b70bda0ad021aec4063641d796fd4d4e91
AhmerRaza12 <ahmer183gamerz@gmail.com>
2024-05-30 01:41:27 +0500

    changes

--- app-service/frontend/src/sections/CakeshSection.jsx
+++ app-service/frontend/src/sections/CakeshSection.jsx
@@ -190,17 +190,17 @@ const CakeshSection = () => {
       <div className="flex items-center justify-center p-12 gap-4">
       <div className="bg-gray-100 h-screen py-8 w-1/2">
     <div className="container mx-auto px-4">
-        <h1 className="text-2xl font-semibold mb-4">Shopping Cart</h1>
+        <h1 className="text-2xl font-semibold mb-4 text-center">Order Details</h1>
         <div className="flex flex-col md:flex-row gap-4">
             
-                <div className="bg-white rounded-lg shadow-md p-6 mb-4 w-full">
+                <div className="bg-white rounded-lg shadow-md p-4 mb- w-full">
                     <table className="w-full">
                         <thead>
                             <tr>
                                 <th className="text-left font-semibold">Product</th>
                                 
                                 <th className="text-left font-semibold">Quantity</th>
-                                <th className="text-left font-semibold">Total</th>
+                                
                             </tr>
                         </thead>
                         <tbody>
@@ -214,12 +214,24 @@ const CakeshSection = () => {
                                 
                                 <td className="py-4">
                                     <div className="flex items-center">
-                                        <button className="border rounded-md py-2 px-4 mr-2">-</button>
-                                        <span className="text-center w-8">1</span>
-                                        <button className="border rounded-md py-2 px-4 ml-2">+</button>
+                                        <button className="py-1 px-2 mr-2 focus:text-lime-400 text-4xl">-</button>
+                                        <span className="text-center w-3">1</span>
+                                        <button className="py-1 px-2 ml-2 focus:text-lime-400 text-4xl text-center">+</button>
                                     </div>
                                 </td>
-                                <td className="py-4">$19.99</td>
+                                <td className="py-4">
+                                <button
+        className="text-slate-800 hover:text-red-500 text-sm bg-white hover:bg-slate-100 border border-slate-200 rounded-lg font-medium px-2 py-2 inline-flex space-x-1 items-center">
+        <span>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
+                stroke="currentColor" className="w-4 h-4">
+                <path strokeLinecap="round" strokeLinejoin="round"
+                    d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+            </svg>
+        </span>
+        <span className="hidden md:inline-block">Delete</span>
+    </button>
+                                </td>
                             </tr>
                             {/* <!-- More product rows --> */}
                         </tbody>
@@ -235,14 +247,14 @@ const CakeshSection = () => {
           
           <div className="flex flex-row gap-2 max-md:flex-none max-md:block">
           <div className="mb-2">
-                <label htmlFor="name" className="mb-3 block text-base font-medium text-[#07074D]">
+                <label htmlFor="name" className="mb-3 block text-base font-medium text-[#62ca54]">
                     Your Name
                 </label>
                 <input type="text" name="name" id="name" placeholder="Full Name"
                     className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
             </div>
             <div className="mb-2">
-                <label htmlFor="phone" className="mb-3 block text-base font-medium text-[#07074D]">
+                <label htmlFor="phone" className="mb-3 block text-base font-medium text-[#62ca54]">
                     Phone Number
                 </label>
                 <input type="text" name="phone" id="phone" placeholder="Enter your phone number"
@@ -252,7 +264,7 @@ const CakeshSection = () => {
           </div>
             
             <div className="mb-2">
-                <label htmlFor="email" className="mb-3 block text-base font-medium text-[#07074D]">
+                <label htmlFor="email" className="mb-3 block text-base font-medium text-[#62ca54]">
                     Email Address
                 </label>
                 <input type="email" name="email" id="email" placeholder="Enter your email"
