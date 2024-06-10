3f28fff0dfdaf7768afac11e2a2fc4624ea594f9
AhmerRaza12 <ahmer183gamerz@gmail.com>
2024-06-11 02:23:11 +0500

    changes

--- app-service/frontend/src/sections/CakeshSection.jsx
+++ app-service/frontend/src/sections/CakeshSection.jsx
@@ -220,68 +220,67 @@ const CakeshSection = () => {
       </section>
       <section className="margin">
         <div className="flex justify-center p-6 gap-4 max-sm:block">
-          <div className="bg-gray-100 py-8 w-1/2 max-sm:w-full">
-            <div className="container mx-auto px-4">
-              <h1 className="text-2xl font-semibold mb-4 text-center">Order Details</h1>
-              <div className="flex flex-col md:flex-row gap-4">
-                <div className="bg-white rounded-lg shadow-md p-4 w-full">
-                  <table className="w-full">
-                    <thead>
-                      <tr>
-                        <th className="text-left font-semibold">Product</th>
-                        <th className="text-left font-semibold">Quantity</th>
-                        <th className="text-left font-semibold">Actions</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {cupcakeslist.map((cupcake, index) => (
-                        <tr key={index}>
-                          <td className="py-4">
-                            <div className="flex items-center">
-                              <img className="h-16 w-16 mr-4" src={cupcakeslist[index]} alt="Product" />
-                              <span className="font-semibold">{cupcakeNameslist[index]}</span>
-                            </div>
-                          </td>
-                          <td className="py-4">
-                            <div className="flex items-center">
-                              <button 
-                                onClick={() => handleDecrement(index)}
-                                className="py-1 px-2 mr-2 focus:text-lime-400 text-4xl"
-                              >
-                                -
-                              </button>
-                              <span className="text-center w-3">{quantities[index]}</span>
-                              <button 
-                                onClick={() => handleIncrement(index)}
-                                className="py-1 px-2 ml-2 focus:text-lime-400 text-4xl text-center"
-                              >
-                                +
-                              </button>
-                            </div>
-                          </td>
-                          <td className="py-4">
-                            <button
-                              onClick={() => handleDelete(index)}
-                              className="text-slate-800 hover:text-red-500 text-sm bg-white hover:bg-slate-100 border border-slate-200 rounded-lg font-medium px-2 py-2 inline-flex space-x-1 items-center"
-                            >
-                              <span>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-4 h-4">
-                                  <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-                                </svg>
-                              </span>
-                              <span className="hidden md:inline-block">Delete</span>
-                            </button>
-                          </td>
-                        </tr>
-                      ))}
-                    </tbody>
-                  </table>
-                </div>
-              </div>
-            </div>
-          </div>
-
-
+        <div className="py-8 w-1/2 max-sm:w-full relative bg-gray-200" >
+          
+  <div className="container mx-auto px-4">
+    <h1 className="text-2xl font-semibold mb-4 text-center">Order Details</h1>
+    <div className="flex flex-col md:flex-row gap-4">
+      <div className="bg-white rounded-lg shadow-md p-4 w-full">
+        <table className="w-full">
+          <thead>
+            <tr>
+              <th className="text-left font-semibold">Product</th>
+              <th className="text-left font-semibold">Quantity</th>
+              <th className="text-left font-semibold">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cupcakeslist.map((cupcake, index) => (
+              <tr key={index}>
+                <td className="py-4">
+                  <div className="flex items-center">
+                    <img className="h-16 w-16 mr-4" src={cupcakeslist[index]} alt="Product" />
+                    <span className="font-semibold">{cupcakeNameslist[index]}</span>
+                  </div>
+                </td>
+                <td className="py-4">
+                  <div className="flex items-center">
+                    <button 
+                      onClick={() => handleDecrement(index)}
+                      className="py-1 px-2 mr-2 focus:text-lime-400 text-4xl"
+                    >
+                      -
+                    </button>
+                    <span className="text-center w-3">{quantities[index]}</span>
+                    <button 
+                      onClick={() => handleIncrement(index)}
+                      className="py-1 px-2 ml-2 focus:text-lime-400 text-4xl text-center"
+                    >
+                      +
+                    </button>
+                  </div>
+                </td>
+                <td className="py-4">
+                  <button
+                    onClick={() => handleDelete(index)}
+                    className="text-slate-800 hover:text-red-500 text-sm bg-white hover:bg-slate-100 border border-slate-200 rounded-lg font-medium px-2 py-2 inline-flex space-x-1 items-center"
+                  >
+                    <span>
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-4 h-4">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                      </svg>
+                    </span>
+                    <span className="hidden md:inline-block">Delete</span>
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
     <div className="mx-auto w-1/2  bg-white max-sm:w-full">
         <form>
           
--- app-service/frontend/tailwind.config.js
+++ app-service/frontend/tailwind.config.js
@@ -11,6 +11,7 @@ const moduleExports = {
       backgroundImage: {
         'about-us': "url('https://images.unsplash.com/photo-1548695607-9c73430ba065?q=80&w=1325&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
         'services':"url('https://plus.unsplash.com/premium_photo-1661881497978-f1f376fa7490?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+        'order':"url('https://images.unsplash.com/photo-1587536849024-daaa4a417b16?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
       }
     },
   },
