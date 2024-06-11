d0f0a6263b9cd1f4f789547782829f79a6edec89
Ashir Ahmed <109139732+ashir-ahmed655@users.noreply.github.com>
2024-06-12 01:06:01 +0500

    Delete frontend directory

--- frontend/src/components/forms/AccountForm.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AccountForm = () => {
-  return (
-    <div>AccountForm</div>
-  )
-}
-
-export default AccountForm
--- frontend/src/components/forms/PickupForm.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const PickupForm = () => {
-  return (
-    <div>PickupForm</div>
-  )
-}
-
-export default PickupForm
